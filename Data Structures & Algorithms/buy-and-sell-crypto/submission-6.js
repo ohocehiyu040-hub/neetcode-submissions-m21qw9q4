a725072bef7d944ffa8fb8af8dcde6e026195760
ohocehiyu040-hub <ohocehiyu040@gmail.com>
2026-03-31 00:42:22 +0300

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0; 
+        let r = 1;
+        let profit = 0
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                const achieve = prices[r] - prices[l];
+                profit = Math.max(profit, achieve);
+                r++
+            }  
+            if (prices[l] >= prices[r]) {
+                l = r; 
+                r++
+            }
+        }
+        return profit
+    }
+}
