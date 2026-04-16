5a38e091af039206d19615233e1695365011ee93
ohocehiyu040-hub <ohocehiyu040@gmail.com>
2026-04-16 14:20:30 +0300

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+        let i = 0; 
+        s.split
+        for (let j = 0; j <= t.length - 1; j++) {
+            if (s[i] === t[j]) {
+                i++
+            } 
+        }
+        return s.length === i
+    }
+}
