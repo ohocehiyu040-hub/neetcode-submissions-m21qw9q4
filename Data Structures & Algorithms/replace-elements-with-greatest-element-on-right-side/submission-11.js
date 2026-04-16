9b04156d525de264718a2fa0bd0be08b71e23384
ohocehiyu040-hub <ohocehiyu040@gmail.com>
2026-04-16 14:03:29 +0300

    Add: replace-elements-with-greatest-element-on-right-side - submission-11

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-11.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let stack = []
+        let maxSoFar = -1; 
+        for (let i = arr.length - 1; i >= 0; i--) {
+    
+            let currentVal = arr[i];
+            arr[i] = maxSoFar
+            maxSoFar = Math.max(maxSoFar, currentVal);
+        }
+        return arr
+    }
+}
